94369ba6ff6c8ea6f601407b490b443d3b68b73a
raulaguilar82 <raulaguilar3089@gmail.com>
2025-04-13 22:14:05 -0400

    feat: implement search functionality and display no results message in the game list

---
+++ controllers/homeController.js
@@ -0,0 +1,16 @@
+const Game = require('../models/Game');
+
+exports.getHome = async (req, res) => {
+    try {
+      const search = req.query.search || ''; // Obtén el término de búsqueda de la query string
+      const query = search
+        ? { title: { $regex: search, $options: 'i' } } // Filtra por título (insensible a mayúsculas/minúsculas)
+        : {};
+      
+      const games = await Game.find(query).sort({ createdAt: -1 }).limit(10);
+      res.render('index', { games, search });
+    } catch (error) {
+      console.error('Error al obtener juegos:', error.message);
+      res.status(500).send('Error interno del servidor');
+    }
+  };
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Game = require('../models/Game'); // Importa el modelo Game
+const homeController = require('../controllers/homeController');
 
-// Ruta raíz
-router.get('/', async (req, res) => {
-  try {
-    const games = await Game.find().sort({ createdAt: -1 }).limit(10);
-    res.render('index', { games });
-  } catch (error) {
-    console.error('Error al obtener juegos:', error.message);
-    res.status(500).send('Error interno del servidor');
-  }
-});
+router.get('/', homeController.getHome);
 
 module.exports = router;
