dc11ad2e09ffc78a0100454a381106ae69a71c78
raulaguilar82 <raulaguilar3089@gmail.com>
2025-04-16 20:04:15 -0400

    fix: Update game detail link to use slug instead of ID for improved routing

--- generatehash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-// Cambia '12345678' por la contraseña que deseas encriptar
-const password = '12345678';
-
-const hashedPassword = bcrypt.hashSync(password, 10); // Genera el hash con un factor de costo de 10
-console.log('Hash generado:', hashedPassword);
